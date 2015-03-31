c6c2b2b2a31dba6d6b7a6594122803d4e7864536
Josh Levine <josh.levine1@gmail.com>
2015-03-31 16:42:16 -0700

    Cleaned up styling on Apods List, Added a header

--- ApodsListComponent.js
+++ ApodsListComponent.js
@@ -23,27 +23,26 @@ class ApodsListComponent extends React.Component {
   }
 
   render() {
-    
-
     return (
       <View style={{flex:1, backgroundColor:'#FFFFFF'}}>
+        <View style={styles.headerSpacer} />
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Recent APODS</Text>
+        </View>
+        <View style={styles.headerSeparator} />
         <ScrollView style={styles.apodsScrollView} contentContainerStyle={styles.apodsContent}>
           {this.props.items.map((item) => {
             var image = item.imageThumbUrl ?
               <Image
                 source={{uri: item.imageThumbUrl}}
-                style={{
-                  width:80,
-                  height:80,
-                  borderRadius:40,
-                  resizeMode: Image.resizeMode.stretch,}}>
+                style={styles.apodItemIcon}>
               </Image> :
               null;
             return (
               <TouchableOpacity onPress={this.onPress(item)}>
-                <View style={{margin:5,width:(Dimensions.get('window').width-20)/2,alignItems:'center',}}>
+                <View style={styles.apodItemWrapper}>
                   {image}
-                  <Text style={{flex:1,color:'#000000'}}>{item.title}</Text>
+                  <Text style={styles.apodItemText}>{item.title}</Text>
                 </View>
               </TouchableOpacity>
             );
@@ -55,6 +54,7 @@ class ApodsListComponent extends React.Component {
 }
 
 var styles = StyleSheet.create({
+  // ScrollView
   apodsScrollView: {
     flex: 1,
     backgroundColor: '#FFFFFF',
@@ -63,7 +63,43 @@ var styles = StyleSheet.create({
     justifyContent:'space-around',
     flexDirection:'row',
     flexWrap:'wrap',
+  },
+  // Header
+  headerSpacer: {
+    height: 20,
+  },
+  header: {
+    height: 30,
+    backgroundColor:'#FEFEFE',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  headerSeparator: {
+    height: 1,
+    backgroundColor:'#F0F0F0',
+  },
+  headerTitle: {
+    fontWeight:'600',
+  },
+  // Apod Item
+  apodItemWrapper: {
+    margin: 5,
+    width: (Dimensions.get('window').width - 20) / 2,
+    alignItems:'center',
+  },
+  apodItemIcon: {
+    width:80,
+    height:80,
+    borderRadius:40,
+    resizeMode: Image.resizeMode.cover,
+  },
+  apodItemText: {
+    fontSize: 12,
+    color:'#000000',
+    flex: 1,
+    marginTop: 5,
   }
+
 });
 
 module.exports = ApodsListComponent;
